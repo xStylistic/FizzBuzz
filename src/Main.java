4454b340d8f818359704b5975a9900f83b720949
xstylistic <bonnybyc@gmail.com>
2024-09-09 18:52:31 -0400

    Merge remote-tracking branch 'origin/main'

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
+
         FizzBuzz.doFizzBuzz();
         FizzBuzz.main();
+
     }
+
 }
--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,5 @@
 public class Multiples {
+
     public static void main(String[] args) {
 
         int count = 0;
