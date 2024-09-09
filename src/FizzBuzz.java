1bbfa5b8bbb81d57d8d5ca7741c422634bde501c
Bonny Chen <bonnychen@bonnys-macbook-pro.local>
2024-09-09 17:42:19 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,13 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        doFizzBuzz();
+    }
+
+    private static void doFizzBuzz() {
+        int i = 1;
+
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -29,6 +35,8 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+
+            i++;
         }
     }
 }
