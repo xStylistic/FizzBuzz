1f789b855c0e7959cdaa3345f1606d7593147fa6
xstylistic <bonnybyc@gmail.com>
2024-09-09 18:35:31 -0400

    Merge remote-tracking branch 'origin/main'

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,7 +2,8 @@ public class Multiples {
     public static void main(String[] args) {
 
         int count = 0;
-        for (int i = 1; i <= 1000; i++) {
+
+        for (int i = 0; i < 1000; i++) {
 
             if (i % 3 == 0 || i % 5 == 0) {
 
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,25 @@
 public class Reduce {
+
     public static void main(String[] args) {
 
+        int count = 0;
+        int n = 100;
+
+        while (n != 0) {
+
+            n = n / 2;
+            count++;
+
+            if (n % 2 == 1) {
+
+                n--;
+
+            }
+
+        }
+
+        System.out.println(count);
+
     }
+
 }
